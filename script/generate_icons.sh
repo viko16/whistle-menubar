483821d846dbefafb8536062acd80346e2b1b990
viko16 <16viko@gmail.com>
2026-05-31 16:04:39 +0800

    chore(resources): compact app icon
    - Rebuild AppIcon.icns with a compact 512px maximum payload
    - Drop duplicate and oversized icon entries from the generator
    - Shrink the bundled icon from 1168202 to 216714 bytes

--- script/generate_icons.sh
+++ script/generate_icons.sh
@@ -21,16 +21,13 @@ render_svg() {
 /bin/rm -rf "$ICONSET_DIR"
 /bin/mkdir -p "$ICONSET_DIR"
 
+# Keep the bundled icon compact; AppIcon.svg remains the source for larger art.
 render_svg "$APP_SVG" 16 "$ICONSET_DIR/icon_16x16.png"
 render_svg "$APP_SVG" 32 "$ICONSET_DIR/icon_16x16@2x.png"
-render_svg "$APP_SVG" 32 "$ICONSET_DIR/icon_32x32.png"
 render_svg "$APP_SVG" 64 "$ICONSET_DIR/icon_32x32@2x.png"
 render_svg "$APP_SVG" 128 "$ICONSET_DIR/icon_128x128.png"
 render_svg "$APP_SVG" 256 "$ICONSET_DIR/icon_128x128@2x.png"
-render_svg "$APP_SVG" 256 "$ICONSET_DIR/icon_256x256.png"
 render_svg "$APP_SVG" 512 "$ICONSET_DIR/icon_256x256@2x.png"
-render_svg "$APP_SVG" 512 "$ICONSET_DIR/icon_512x512.png"
-render_svg "$APP_SVG" 1024 "$ICONSET_DIR/icon_512x512@2x.png"
 render_svg "$STATUS_SVG" 72 "$RESOURCE_DIR/StatusBarIconTemplate.png"
 
 /usr/bin/python3 - "$ICONSET_DIR" "$RESOURCE_DIR/AppIcon.icns" <<'PY'
@@ -42,17 +39,11 @@ iconset = Path(sys.argv[1])
 output = Path(sys.argv[2])
 entries = [
     ("icp4", "icon_16x16.png"),
-    ("icp5", "icon_16x16@2x.png"),
     ("ic11", "icon_16x16@2x.png"),
-    ("ic04", "icon_32x32.png"),
-    ("icp6", "icon_32x32@2x.png"),
     ("ic12", "icon_32x32@2x.png"),
     ("ic07", "icon_128x128.png"),
-    ("ic08", "icon_128x128@2x.png"),
-    ("ic09", "icon_256x256@2x.png"),
-    ("ic13", "icon_256x256@2x.png"),
-    ("ic10", "icon_512x512@2x.png"),
-    ("ic14", "icon_512x512@2x.png"),
+    ("ic13", "icon_128x128@2x.png"),
+    ("ic14", "icon_256x256@2x.png"),
 ]
 
 body = bytearray()
